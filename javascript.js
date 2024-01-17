66d380038ab787d81e42a40ccd3ddf88af2cf4a4
AlEkO234 <151035310+AlEkO234@users.noreply.github.com>
2024-01-17 08:28:28 -0800

    gffgf

---
+++ javascript.js
@@ -0,0 +1,39 @@
+//pirveli
+let number = 100
+
+if (number < 50)
+    console.log
+{ ('ნაკლებია 50 ზე'); }
+else if (number > 20)
+    console.log{ ("მეტია 20 ზე"); }
+else { ("შეცდომა") }
+
+//ternary
+
+let secondnumb = 100 < 50 ? "ნაკლებია 50 ზე" : 100 > 20 ? "მეტია 20 ზე" : "შეცდომა"
+console.log(secondnumb);//"მეტია 20 ზე" 
+
+//meore
+let name = "მარიამი"
+
+if (name == "მარიამი") { console.log('true') }
+else { console.log("false") }
+//mesame 
+
+let Name = 'მარიამი'
+
+switch (Name) {
+    case 'მარიამი':
+        console.log('true');
+        break;
+
+    default:
+        console.logS("false");
+}
+//meotxe
+let string= 'სიტყვა';
+console.log(typeof String )
+//mexute
+
+let number = 10
+console.log(string(number))
